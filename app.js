52bc4b082c2ec8aa545cb8b978d411933c9f84a4
Vusal Dadashov <61565815+vusaldadashov@users.noreply.github.com>
2023-10-07 21:37:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET","POST"],
   credentials: true
 }))
