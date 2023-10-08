c2459076d41dd553fd701c43abe2c329205d3c4f
Vusal Dadashov <61565815+vusaldadashov@users.noreply.github.com>
2023-10-08 16:09:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(cors({
-  origin: "*",
+  origin: "https://movie-website-app.onrender.com/",
   methods: ["GET","POST"],
   credentials: true
 }))
